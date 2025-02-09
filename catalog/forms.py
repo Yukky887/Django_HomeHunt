99ff7674fd14ab46c39c3d61af44f218af79e306
Yukky887 <egorka0303@inbox.ru>
2025-02-09 17:39:49 +0300

    Добавлена проверка на индивидуальность Email при регистрации

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,18 +4,12 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
-class CustomUserCreationForm(forms.ModelForm):
     password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
     password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ["username", "email"]
+        fields = ['username', 'email', 'password1', 'password2']
 
     def clean_email(self):
         email = self.cleaned_data.get("email")
